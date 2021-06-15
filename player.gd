d7946ff5334cf25da1e57bf785ef74d39a47dc19
4k437 <79691709+4k437@users.noreply.github.com>
2021-06-15 12:07:03 +0300

    gravity

--- player.gd
+++ player.gd
@@ -4,6 +4,9 @@ export var mouse_sensitivity = 1.0;
 onready var camera = $camera
 
 export var move_speed = 20;
+export var gravity = 20.0;
+var current_gravity = 0.0;
+export var jump_force = 5.0;
 
 func _input(event):         
 	if event is InputEventMouseMotion:
@@ -19,4 +22,16 @@ func _physics_process(delta):
 	).normalized()
 	velocity = velocity.x * transform.basis.x + velocity.z * transform.basis.z
 	velocity *= move_speed
+	current_gravity -= gravity * delta
+	velocity.y = current_gravity
 	move_and_slide(velocity)
+	
+	if (is_on_floor()):
+		current_gravity = 0
+		if (Input.is_action_just_pressed("move_jump")):
+			current_gravity = jump_force
+			print("jumping")
+	
+	if (Input.is_action_just_pressed("move_jump")):
+		print(current_gravity)
+	
