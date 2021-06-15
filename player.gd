eefccb9cfd041aacd9c9aa77f20e4dbf6a06c946
4kurs437 <79691709+4k437@users.noreply.github.com>
2021-06-15 14:52:06 +0300

    jumping

--- player.gd
+++ player.gd
@@ -15,23 +15,20 @@ func _input(event):
 		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
 
 func _physics_process(delta):
-	var velocity = Vector3(
+	var velocity = Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		0,
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	).normalized()
-	velocity = velocity.x * transform.basis.x + velocity.z * transform.basis.z
+	
+	velocity = velocity.x * transform.basis.x + velocity.y * transform.basis.z
 	velocity *= move_speed
+	
 	current_gravity -= gravity * delta
 	velocity.y = current_gravity
-	move_and_slide(velocity)
+	
+	move_and_slide(velocity, Vector3.UP)
 	
 	if (is_on_floor()):
 		current_gravity = 0
 		if (Input.is_action_just_pressed("move_jump")):
 			current_gravity = jump_force
-			print("jumping")
-	
-	if (Input.is_action_just_pressed("move_jump")):
-		print(current_gravity)
-	
