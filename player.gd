edf7b484954e6a7f3ede97050d8cd5a4e79e0d7e
4k437 <79691709+4k437@users.noreply.github.com>
2021-06-15 10:38:02 +0300

    player and movement

--- player.gd
+++ player.gd
@@ -1,4 +1,12 @@
 extends KinematicBody
 
-func _ready():
-	pass
+export var move_speed = 20;
+
+func _physics_process(delta):
+	var velocity = Vector3(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		0,
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	).normalized()
+	velocity *= move_speed
+	move_and_slide(velocity)
