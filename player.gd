44ba4285a820bf355e85bbf88dc18d348b07145f
4k437 <79691709+4k437@users.noreply.github.com>
2021-06-15 10:53:04 +0300

    player camera, environment

--- player.gd
+++ player.gd
@@ -1,6 +1,15 @@
 extends KinematicBody
 
+export var mouse_sensitivity = 1.0;
+onready var camera = $camera
+
 export var move_speed = 20;
+
+func _input(event):         
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= mouse_sensitivity * event.relative.x
+		camera.rotation_degrees.x -= mouse_sensitivity * event.relative.y
+		camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -90, 90)
 
 func _physics_process(delta):
 	var velocity = Vector3(
@@ -8,5 +17,6 @@ func _physics_process(delta):
 		0,
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	).normalized()
+	velocity = velocity.x * transform.basis.x + velocity.z * transform.basis.z
 	velocity *= move_speed
 	move_and_slide(velocity)
