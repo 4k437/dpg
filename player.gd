b983849decaf7fef2b8318ff80641f48d6c2dece
4kurs437 <79691709+4k437@users.noreply.github.com>
2021-06-16 22:26:50 +0300

    momentum movement

--- player.gd
+++ player.gd
@@ -3,10 +3,15 @@ extends KinematicBody
 export var mouse_sensitivity = 1.0;
 onready var camera = $camera
 
-export var move_speed = 20;
-export var gravity = 20.0;
-var current_gravity = 0.0;
-export var jump_force = 5.0;
+var velocity:Vector3
+var current_gravity = 0.0
+export var move_speed = 5
+export var acceleration = 0.1
+export var friction = 0.1
+export var gravity = 20.0
+export var jump_force = 5.0
+
+var is_grounded = false
 
 func _ready() -> void:
 	Timekeeper.connect("tick", self, "tick")
@@ -27,23 +32,29 @@ func _physics_process(delta):
 		global_transform.origin = info[0]
 		rotation = info[1]
 		camera.rotation = info[2]
-		current_gravity = info[3]
+		velocity = info[3]
+		is_grounded = info[4]
 
 func movement(delta):
-	var velocity = Vector2(
+	var move_input = Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	).normalized()
 	
-	velocity = velocity.x * transform.basis.x + velocity.y * transform.basis.z
-	velocity *= move_speed
+	if (is_grounded):
+		if (move_input == Vector2.ZERO):
+			velocity = velocity.linear_interpolate(Vector3.ZERO, friction)
+		else:
+			var global_move_input = (move_input.x * transform.basis.x + move_input.y * transform.basis.z)
+			velocity = velocity.linear_interpolate(global_move_input * move_speed, acceleration)
 	
 	current_gravity -= gravity * delta
 	velocity.y = current_gravity
 	
-	move_and_slide(velocity, Vector3.UP)
+	velocity = move_and_slide(velocity, Vector3.UP)
 	
-	if (is_on_floor()):
+	is_grounded = is_on_floor()
+	if (is_grounded):
 		current_gravity = 0
 		if (Input.is_action_just_pressed("move_jump")):
 			current_gravity = jump_force
@@ -53,6 +64,7 @@ func tick():
 		global_transform.origin,
 		rotation,
 		camera.rotation,
-		current_gravity
+		velocity,
+		is_grounded
 	]
 	Timekeeper.store_information(info, self)
