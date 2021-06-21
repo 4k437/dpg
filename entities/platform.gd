57d0b2d8ef5db06d541190fe489bbbc8dfc18f55
4kurs437 <79691709+4k437@users.noreply.github.com>
2021-06-22 02:39:30 +0300

    level 1

--- entities/platform.gd
+++ entities/platform.gd
@@ -4,6 +4,7 @@ var point1:Vector3
 export var point2:Vector3
 
 export var move_speed = 1.0
+export var time_offset = 0.0;
 
 func _ready() -> void:
 	point1 = transform.origin
@@ -13,7 +14,7 @@ func _physics_process(delta: float) -> void:
 	var distance = point1.distance_to(point2)
 	transform.origin = point1.linear_interpolate(
 		point2,
-		triangle_wave(Timekeeper.current_time as float / distance / 100 * move_speed)
+		triangle_wave((Timekeeper.current_time + time_offset) / distance / 100 * move_speed)
 	)
 
 func triangle_wave(time:float) -> float:
