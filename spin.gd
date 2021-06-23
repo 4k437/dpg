cc45e0c2d1f88b6643ede2dac61071b85117c886
4kurs437 <79691709+4k437@users.noreply.github.com>
2021-06-24 00:22:52 +0300

    randomize rotation and delta speed

--- spin.gd
+++ spin.gd
@@ -2,8 +2,12 @@ extends Control
 
 export var speed := 1.0
 
+func _ready() -> void:
+	randomize()
+	rect_rotation += randi() % 360
+
 func _process(delta):
 	var mult = 1
 	if (Input.is_action_pressed("rewind")):
 		mult = -2
-	rect_rotation += speed * mult
+	rect_rotation += speed * mult * delta
